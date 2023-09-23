d20ea611a3f68ee883784df1617a17e2b51194e0
AadiManchekar <aadimanchekar2002@gmail.com>
2023-09-23 04:14:48 -0700

    ProductService starter

---
+++ ProductService/src/main/java/com/aadi/ProductService/service/implementation/ProductServiceImpl.java
@@ -0,0 +1,10 @@
+package com.aadi.ProductService.service.implementation;
+
+import org.springframework.stereotype.Service;
+
+import com.aadi.ProductService.service.ProductService;
+
+@Service
+public class ProductServiceImpl implements ProductService{
+    
+}
